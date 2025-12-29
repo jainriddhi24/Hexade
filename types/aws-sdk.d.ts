c26a18fedc9612475f3418dde10789ea8425a8d2
Riddhi <riddhicjain242@gmail.com>
2025-12-29 16:02:13 +0530

    Fix aws-sdk type declarations with proper S3 namespace and methods

--- types/aws-sdk.d.ts
+++ types/aws-sdk.d.ts
@@ -1,1 +1,36 @@
-declare module 'aws-sdk';
+declare module 'aws-sdk' {
+  export namespace S3 {
+    interface PutObjectRequest {
+      Bucket: string;
+      Key: string;
+      Body: any;
+      ContentType?: string;
+      [key: string]: any;
+    }
+    interface GetObjectRequest {
+      Bucket: string;
+      Key: string;
+      [key: string]: any;
+    }
+    interface DeleteObjectRequest {
+      Bucket: string;
+      Key: string;
+      [key: string]: any;
+    }
+  }
+  
+  export class S3 {
+    constructor(options?: any);
+    putObject(params: S3.PutObjectRequest, callback?: (err: any, data: any) => void): any;
+    getObject(params: S3.GetObjectRequest, callback?: (err: any, data: any) => void): any;
+    deleteObject(params: S3.DeleteObjectRequest, callback?: (err: any, data: any) => void): any;
+    [key: string]: any;
+  }
+  
+  const awsSdk: {
+    S3: typeof S3;
+    [key: string]: any;
+  };
+  
+  export default awsSdk;
+}
